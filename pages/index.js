2090fb5bbcfa048419d9760a36fd9e8c3cda75cb
MIYABETaisei <mytspe46@gmail.com>
2022-04-18 22:58:22 +0900

    change message

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,10 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Hello Taisei!
+        </h1>
+        <h1 className={styles.title}>
+          Welcome o <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
