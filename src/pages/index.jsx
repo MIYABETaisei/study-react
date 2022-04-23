a8c625077fb6a8fc62a86b1fd1b75d52b723f864
MIYABETaisei <mytspe46@gmail.com>
2022-04-23 10:27:43 +0900

    ✨ カウンターを設置してuseStateでカウントアップする

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,35 +3,31 @@ import styles from "src/styles/Home.module.css";
 import { Footer } from "src/components/Footer";
 import Main from "src/components/Main";
 import { Header } from "src/components/Header";
-import Link from "next/link";
-import { useCallback, useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  const foo = 1;
-  const handleClip = useCallback((e) => {
-    console.log(e.target.href);
-    e.preventDefault();
-    alert(foo);
-  }, []);
+  const [count, setCount] = useState(1)
 
+  const handleClick = (e) => {
+    setCount((count) => count + 1);
+  };
+  
   useEffect(() => {
-    console.log("マウント時");
     document.body.style.backgroundColor = "lightblue";
     return () => {
-      console.log("アンマウント時");
       document.body.style.backgroundColor = "";
     };
   }, []);
-
+  
+  console.log(count);
   return (
     <div className={styles.container}>
       <Head>
         <title>Index Page</title>
       </Head>
       <Header />
-      <Link href="/about">
-        <a onClick={handleClip}>ボタン</a>
-      </Link>
+      <h1>{count}</h1>
+      <button onClick={handleClick}>ボタン</button>
       <Main page="index" />
       <Footer />
     </div>
