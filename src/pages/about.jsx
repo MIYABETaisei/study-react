490248ffb452347a71c3e148fed10df81f6e4da7
MIYABETaisei <mytspe46@gmail.com>
2022-04-23 16:22:44 +0900

    ✨ ページ間のStateのリフトアップについて勉強

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -3,14 +3,17 @@ import { Footer } from "src/components/Footer";
 import { Header } from "src/components/Header";
 import Main from "src/components/Main";
 import styles from "src/styles/Home.module.css";
-import { useCounter } from "src/hooks/useCounter";
-import { useInputArray } from "src/hooks/useInputArray";
-import { useBgLightBlue } from "src/hooks/useBgLightBlue";
 
-export default function About() {
-  const { count, isShow, handleClick, handleDisplay } = useCounter();
-  const { text, array, handleAdd, handleChange } = useInputArray();
-  useBgLightBlue();
+export default function About({
+    count,
+    isShow,
+    handleClick,
+    handleDisplay,
+    text,
+    array,
+    handleAdd,
+    handleChange,
+  }) {
 
   return (
     <div className={styles.container}>
