8f8b6517600732cbde1e3fe8ccafba92b125c94e
MIYABETaisei <mytspe46@gmail.com>
2022-04-23 10:00:36 +0900

    ✨ 説明用のuseEffectを導入

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,13 +3,8 @@ import styles from "src/styles/Home.module.css";
 import { Footer } from "src/components/Footer";
 import Main from "src/components/Main";
 import { Header } from "src/components/Header";
-import { useCallback } from "react";
 import Link from "next/link";
-
-// const handleClip = (e) => {
-//   console.log(e.target.href);
-//   e.preventDefault();
-// };
+import { useCallback, useEffect } from "react";
 
 export default function Home() {
   const foo = 1;
@@ -18,6 +13,16 @@ export default function Home() {
     e.preventDefault();
     alert(foo);
   }, []);
+
+  useEffect(() => {
+    console.log("マウント時");
+    document.body.style.backgroundColor = "lightblue";
+    return () => {
+      console.log("アンマウント時");
+      document.body.style.backgroundColor = "";
+    };
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
