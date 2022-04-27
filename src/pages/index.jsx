8d8caa776ee024e41e0919bfb4e3bf82703adeb5
MIYABETaisei <mytspe46@gmail.com>
2022-04-27 16:45:17 +0900

    🔥 indexから不要なコードを削除

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,32 +4,15 @@ import { Header } from "src/components/Header";
 import { Main } from "src/components/Main";
 import styles from "src/styles/Home.module.css";
 
-const About = (props) => {
+const About = () => {
   return (
     <div className={styles.container}>
       <Head>
-        <title>About Page</title>
+        <title>Index Page</title>
       </Head>
       <Header />
-      {props.isShow ? <h1>{props.doubleCount}</h1> : null}
-      <hr />
-      <button onClick={props.handleClick}>ボタン</button>
-      <button onClick={props.handleDisplay}>
-        {props.isShow ? "非表示" : "表示"}
-      </button>
-      <input
-        type="text"
-        value={props.text}
-        onChange={props.handleChange}
-      ></input>
-      <button onClick={props.handleAdd}>追加</button>
-      <ul>
-        {props.array.map((item) => {
-          return <li key={item}>{item}</li>;
-        })}
-      </ul>
-      <Main page="about" />
-      <Footer />
+      <h1>Next.jsで学ぶReact講座</h1>
+      <p>JSONplaceholderのAPIを色々叩いてみるよ！</p>
     </div>
   );
 };
